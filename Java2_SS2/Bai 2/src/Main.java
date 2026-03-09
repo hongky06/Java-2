abf615e311ca011024f5a6a4c36d54553b945a2d
hongky06 <dohongky2006@gmail.com>
2026-03-09 19:20:32 +0700

    Add files via upload

---
+++ Java2_SS2/Bai 2/src/Main.java
@@ -0,0 +1,11 @@
+@FunctionalInterface
+interface PassworldValidator{
+    boolean isValid(String password);
+}
+public class Main {
+    public static void main(String[] args) {
+        PassworldValidator validator = password -> password.length() >= 8;
+        System.out.println(validator.isValid("12345678"));
+        System.out.println(validator.isValid("12345"));
+    }
+}
